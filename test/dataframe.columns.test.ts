f5c03a35bb1a8c604088884d1e23db903dea1d11
Ashley Davis <ashley@codecapers.com.au>
2018-01-05 14:27:06 +1000

    Moved a test from JS implementation.

--- test/dataframe.columns.test.ts
+++ test/dataframe.columns.test.ts
@@ -168,4 +168,39 @@ describe('DataFrame columns', () => {
 
         expect(dataFrame.getColumnNames()).to.eql(["X", "Y"]);
     });
+
+	it('creating from objects with variable fields - by default just uses first row to determine column names', function () {
+		
+		var dataFrame = new DataFrame({
+			values: [
+				{ c1: 1, c2: 2 },
+				{ c3: 3, c4: 4 },
+			],
+		});
+
+		var columnNames = ["c1", "c2"];
+		expect(dataFrame.getColumnNames()).to.eql(columnNames);
+		expect(dataFrame.toArray()).to.eql([
+			{ c1: 1, c2: 2 },
+			{ c3: 3, c4: 4 },
+		]);
+	});
+
+	it('creating from objects with variable fields - can force all rows to be considered to determine column names', function () {
+		
+		var dataFrame = new DataFrame({
+			values: [
+				{ c1: 1, c2: 2 },
+				{ c3: 3, c4: 4 },
+			],
+			considerAllRows: true,
+		});
+
+		var columnNames = ["c1", "c2", "c3", "c4"];
+		expect(dataFrame.getColumnNames()).to.eql(columnNames);
+		expect(dataFrame.toPairs()).to.eql([
+			[0, { c1: 1, c2: 2 }],
+			[1, { c3: 3, c4: 4 }],
+		]);
+	});
 });
