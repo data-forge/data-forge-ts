c800c18fa0adc2c626d8b618b0f561315d5ed024
Ashley Davis <ashley@codecapers.com.au>
2018-01-11 17:19:08 +1000

    Moved tests over from JS implementation.

--- build/test/dataframe.series.test.js
+++ build/test/dataframe.series.test.js
@@ -38,5 +38,32 @@ describe('DataFrame', function () {
         });
         chai_1.expect(dataFrame.getSeries("B").getIndex().toArray()).to.eql([100, 200]);
     });
+    it('when a series is extracted from a dataframe, undefined values are stripped out.', function () {
+        var dataFrame = new dataframe_1.DataFrame({
+            columnNames: ["S"],
+            values: [
+                [undefined],
+                [11],
+                [undefined],
+                [12],
+                [undefined],
+            ]
+        });
+        var series = dataFrame.getSeries('S');
+        chai_1.expect(series.toPairs()).to.eql([
+            [1, 11],
+            [3, 12],
+        ]);
+    });
+    it('retreive a non-existing column results in an empty series', function () {
+        var dataFrame = new dataframe_1.DataFrame({
+            columnNames: ["C1"],
+            values: [
+                [1]
+            ],
+        });
+        var series = dataFrame.getSeries("non-existing-column");
+        chai_1.expect(series.toPairs()).to.eql([]);
+    });
 });
 //# sourceMappingURL=dataframe.series.test.js.map
--- test/dataframe.series.test.ts
+++ test/dataframe.series.test.ts
@@ -46,4 +46,36 @@ describe('DataFrame', () => {
         expect(dataFrame.getSeries("B").getIndex().toArray()).to.eql([100, 200]);
     });
 
+	it('when a series is extracted from a dataframe, undefined values are stripped out.', function () {
+		
+		var dataFrame = new DataFrame({
+			columnNames: [ "S" ],
+			values: [
+				[undefined],
+				[11],
+				[undefined],
+				[12],
+				[undefined],
+			]
+        });
+		
+		var series = dataFrame.getSeries('S');
+		expect(series.toPairs()).to.eql([
+			[1, 11],
+			[3, 12],
+		]);
+    });
+
+	it('retreive a non-existing column results in an empty series', function () {
+
+		var dataFrame = new DataFrame({
+			columnNames: ["C1"],
+			values: [
+				[1]
+			],
+		});
+
+		var series = dataFrame.getSeries("non-existing-column");
+		expect(series.toPairs()).to.eql([]);
+	});
 });
