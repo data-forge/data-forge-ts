969635bf11252541934d395237b8e19b526c52ef
Ashley Davis <ashley@codecapers.com.au>
2018-04-19 13:09:07 +1000

    More tests for DataFrame.

--- src/test/dataframe.window.test.ts
+++ src/test/dataframe.window.test.ts
@@ -1,10 +1,11 @@
 import { assert, expect } from 'chai';
 import 'mocha';
+import * as dataForge from '../index';
 import { DataFrame, Series, ISeries, Index } from '../index';
 
 describe('DataFrame window', () => {
     
-	it('can compute window - creates an empty series from an empty data set', function () {
+	it('can compute window - creates an empty series from an empty data set', () => {
 
 		var dataframe = new DataFrame();
 		var windowed = dataframe.window(2)
@@ -18,7 +19,7 @@ describe('DataFrame window', () => {
 		expect(windowed.count()).to.eql(0);
 	});
 
-	it('can compute window - with even window size and even number of rows', function () {
+	it('can compute window - with even window size and even number of rows', () => {
 
         var dataframe = new DataFrame({ index: [10, 20, 30, 40], values: [1, 2, 3, 4] });
 		var windowed = dataframe
@@ -38,7 +39,7 @@ describe('DataFrame window', () => {
 		]);
 	});
 
-	it('can compute window - with even window size and odd number of rows', function () {
+	it('can compute window - with even window size and odd number of rows', () => {
 
 		var dataframe = new DataFrame({ index: [10, 20, 30, 40, 50], values: [1, 2, 3, 4, 5] });
 		var windowed = dataframe
@@ -59,7 +60,7 @@ describe('DataFrame window', () => {
 		]);
 	});
 
-	it('can compute window - with odd window size and odd number of rows', function () {
+	it('can compute window - with odd window size and odd number of rows', () => {
 
 		var dataframe = new DataFrame({ values: [1, 2, 3, 4, 5, 6] });
 		var windowed = dataframe
@@ -80,7 +81,7 @@ describe('DataFrame window', () => {
 
 	});
 
-	it('can compute window - with odd window size and even number of rows', function () {
+	it('can compute window - with odd window size and even number of rows', () => {
 
 		var dataframe = new DataFrame({ values: [1, 2, 3, 4, 5] });
 		var windowed = dataframe
@@ -102,7 +103,7 @@ describe('DataFrame window', () => {
 	});
 
     
-	it('can compute rolling window - from empty data set', function () {
+	it('can compute rolling window - from empty data set', () => {
 
 		var dataframe = new DataFrame();
 		var windowed = dataframe
@@ -119,7 +120,7 @@ describe('DataFrame window', () => {
 		expect(windowed.toArray().length).to.eql(0);
 	});
 
-	it('rolling window returns 0 values when there are not enough values in the data set', function () {
+	it('rolling window returns 0 values when there are not enough values in the data set', () => {
 
 		var dataframe = new DataFrame({ index: [0, 1], values: [1, 2] });
 		var windowed = dataframe
@@ -136,7 +137,7 @@ describe('DataFrame window', () => {
 		expect(windowed.toArray().length).to.eql(0);
 	});
 
-	it('can compute rolling window - odd data set with even period', function () {
+	it('can compute rolling window - odd data set with even period', () => {
 
 		var dataframe = new DataFrame({ index: [10, 20, 30, 40, 50], values: [0, 1, 2, 3, 4] });
 		var windowed = dataframe
@@ -157,7 +158,7 @@ describe('DataFrame window', () => {
         ]);
 	});
 
-	it('can compute rolling window - odd data set with odd period', function () {
+	it('can compute rolling window - odd data set with odd period', () => {
 
 		var dataframe = new DataFrame({ index: [0, 1, 2, 3, 4], values: [0, 1, 2, 3, 4] });
 		var windowed = dataframe
@@ -181,7 +182,7 @@ describe('DataFrame window', () => {
 		expect(values[2]).to.eql([2, 3, 4]);
 	});
 
-	it('can compute rolling window - even data set with even period', function () {
+	it('can compute rolling window - even data set with even period', () => {
 
 		var dataframe = new DataFrame({ index: [0, 1, 2, 3, 4, 5], values: [0, 1, 2, 3, 4, 5] });
 		var windowed = dataframe
@@ -207,7 +208,7 @@ describe('DataFrame window', () => {
 		expect(values[4]).to.eql([4, 5]);
 	});
 
-	it('can compute rolling window - even data set with odd period', function () {
+	it('can compute rolling window - even data set with odd period', () => {
 
 		var dataframe = new DataFrame({ index: [0, 1, 2, 3, 4, 5], values: [0, 1, 2, 3, 4, 5] });
 		var windowed = dataframe
@@ -232,7 +233,7 @@ describe('DataFrame window', () => {
 		expect(values[3]).to.eql([3, 4, 5]);
 	});
 
-	it('can compute rolling window - can take last index and value from each window', function () {
+	it('can compute rolling window - can take last index and value from each window', () => {
 
 		var dataframe = new DataFrame({ index: [0, 1, 2, 3, 4, 5], values: [0, 1, 2, 3, 4, 5] });
 		var windowed = dataframe
@@ -253,7 +254,7 @@ describe('DataFrame window', () => {
 		expect(values).to.eql([2, 3, 4, 5]);
 	});
 
-	it('variable window', function () {
+	it('variable window', () => {
 
 		var dataframe = new DataFrame({ 
 			index:  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -282,7 +283,7 @@ describe('DataFrame window', () => {
 		]);
 	});
     
-	it('can collapse sequential duplicates and take first index', function () {
+	it('can collapse sequential duplicates and take first index', () => {
 
 		var dataframe = new DataFrame({ 
 			index:  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -301,7 +302,7 @@ describe('DataFrame window', () => {
 		]);
 	});
 
-	it('can collapse sequential duplicates with custom selector', function () {
+	it('can collapse sequential duplicates with custom selector', () => {
 
 		var dataframe = new DataFrame({ 
 			index:  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -322,4 +323,144 @@ describe('DataFrame window', () => {
 			[9, 7],
 		]);
     });
+
+	it('rolling window', () => {
+
+		var dataFrame = new DataFrame({
+            columns: {
+                Value: dataForge.range(1, 12)
+            },
+            index: dataForge.range(10, 12),
+		});
+
+		var newSeries = dataFrame.getSeries('Value')
+			.rollingWindow(5)
+			.asPairs()
+			.select(function (pair) {
+				var windowIndex = pair[0]
+				var window = pair[1];
+				return [window.getIndex().last(), window.last()];
+			})
+			.asValues<number, any>()
+			;
+
+		expect(newSeries.getIndex().toArray()).to.eql([14, 15, 16, 17, 18, 19, 20, 21]);
+		expect(newSeries.toArray()).to.eql([5, 6, 7, 8, 9, 10, 11, 12]);
+
+		var newDataFrame = dataFrame.withSeries('Value2', newSeries);
+
+		expect(newDataFrame.getIndex().toArray()).to.eql([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]);
+
+		expect(newDataFrame.toRows()).to.eql([
+			[1, undefined],
+			[2, undefined],
+			[3, undefined],
+			[4, undefined],
+			[5, 5],
+			[6, 6],
+			[7, 7],
+			[8, 8],
+			[9, 9],
+			[10, 10],
+			[11, 11],
+			[12, 12],
+		]);
+
+	});
+
+	function initDataFrame (columns: any[], rows: any[], index?: any[]) {
+		assert.isArray(columns);
+		assert.isArray(rows);
+		
+		var config: any = {
+			columnNames: columns,
+			rows: rows,			
+		};
+
+		if (index) {
+			config.index = index;
+		}
+
+		return new DataFrame(config);
+	}
+
+	//
+	// Generate a data frame for testing.
+	//
+	function genDataFrame (numColumns: number, numRows: number) {
+
+        const columnNames: string[] = [];
+        for (let i = 0; i < numColumns; ++i) {
+            columnNames.push((i+1).toString());
+        }
+
+        const rows: any[] = [];
+        for (let i = 0; i < numRows; ++i) {
+            const row: any[] = [];
+            for (let j = 0; j < numColumns; ++j) {
+                row.push((i+1) * (j+1));
+            }
+            rows.push(row);
+        }
+        
+		return initDataFrame(columnNames, rows);
+	}
+
+	it('can use window and selectMany to generate multiple elements', () => {
+
+		var dataFrame = genDataFrame(2, 4);
+		var series = dataFrame
+			.window(2)
+			.asPairs()
+			.select((pair: any) => {
+				var windowIndex = pair[0]
+				var window = pair[1];
+				return [windowIndex, [window.getSeries("1").sum(), window.getSeries("2").sum()]];
+			})
+			.asValues()
+			.selectMany((value: any) => {
+				assert.isArray(value);
+				return value; // The value is already a list.
+			});
+
+		expect(series.toPairs()).to.eql([
+			[0, 3],
+			[0, 6],
+			[1, 7],
+			[1, 14],
+		]);
+	});
+
+	it('can use rollingWindow and selectMany to generate multiple elements', () => {
+
+		var dataFrame = genDataFrame(2, 4);
+		var series = dataFrame
+			.rollingWindow(2)
+			.asPairs()
+			.select((pair: any) => {
+				var windowIndex = pair[0]
+				var window = pair[1];
+				return [
+					windowIndex, 
+					[
+						window.getSeries("1").sum(), 
+						window.getSeries("2").sum()
+					]
+				];
+			})
+			.asValues()
+			.selectMany((value: any) => {
+				assert.isArray(value);
+				return value; // The value is already a list.
+			});
+
+		expect(series.toPairs()).to.eql([
+			[0, 3],
+			[0, 6],
+			[1, 5],
+			[1, 10],
+			[2, 7],
+			[2, 14],
+		]);
+	});
 });
