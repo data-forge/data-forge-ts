c1bdc6a2542c8a9247c5f1356200102e77d43439
Ashley Davis <ashley@codecapers.com.au>
2018-05-26 13:01:31 +1000

    Added tests for grouping by single and multiple columns.

--- src/test/dataframe.group.test.ts
+++ src/test/dataframe.group.test.ts
@@ -38,6 +38,116 @@ describe('DataFrame group', () => {
 		]);
     });
     
+	it('can group by single column', function () {
+		var df = new DataFrame([
+            {
+                id: 0,
+                A: 1,
+            },
+            {
+                id: 10,
+                A: 2,
+            },
+            {
+                id: 20,
+                A: 3,
+            },
+            {
+                id: 30,
+                A: 2,
+            },
+            {
+                id: 40,
+                A: 2,
+            },
+            {
+                id: 50,
+                A: 1,
+            },            
+        ]);
+
+        var grouped = df.groupBy(row => row.A);
+		expect(grouped.count()).to.eql(3);
+
+		var group1 = grouped.skip(0).first();
+		expect(group1.toArray()).to.eql([
+            { id: 0, A: 1 },
+            { id: 50, A: 1 },
+		]);
+
+		var group2 = grouped.skip(1).first();
+		expect(group2.toArray()).to.eql([
+            { id: 10, A: 2 },
+            { id: 30, A: 2 },
+            { id: 40, A: 2 },
+		]);
+
+		var group3 = grouped.skip(2).first();
+		expect(group3.toArray()).to.eql([
+            { id: 20, A: 3 },
+		]);
+    });
+
+	it('can group by multiple column', function () {
+		var df = new DataFrame([
+            {
+                id: 0,
+                A: 1,
+                B: 100,
+            },
+            {
+                id: 10,
+                A: 2,
+                B: 200,
+            },
+            {
+                id: 20,
+                A: 3,
+                B: 300,
+            },
+            {
+                id: 30,
+                A: 2,
+                B: 200,
+            },
+            {
+                id: 40,
+                A: 2,
+                B: 400,
+            },
+            {
+                id: 50,
+                A: 1,
+                B: 100,
+            },
+        ]);
+
+        var grouped = df.groupBy(row => [row.A, row.B]);
+		expect(grouped.count()).to.eql(4);
+
+		var group1 = grouped.skip(0).first();
+		expect(group1.toArray()).to.eql([
+            { id: 0, A: 1, B: 100 },
+            { id: 50, A: 1, B: 100 },
+		]);
+
+		var group2 = grouped.skip(1).first();
+		expect(group2.toArray()).to.eql([
+            { id: 10, A: 2, B: 200 },
+            { id: 30, A: 2, B: 200 },
+		]);
+
+		var group3 = grouped.skip(2).first();
+		expect(group3.toArray()).to.eql([
+            { id: 20, A: 3, B: 300 },
+        ]);
+
+		var group4 = grouped.skip(3).first();
+		expect(group4.toArray()).to.eql([
+            { id: 40, A: 2, B: 400 },
+        ]);
+    });
+    
 	it('can group sequential duplicates and take first index', function () {
 		var df = new DataFrame({ 
 			index:  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
