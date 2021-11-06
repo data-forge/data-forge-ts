4916c7d846b24463d0a82bebe1809b9b473c3743
Ashley Davis <ashley@codecapers.com.au>
2021-11-06 11:48:27 +1000

    Implemented DataFrame.map. DataFrame.select is now an alias for map.

--- src/test/series.map.test.ts
+++ src/test/series.map.test.ts
@@ -8,46 +8,46 @@ describe('Series.map', () => {
 
     it('can rewrite series with map', () => {
 
-        var series = new Series([10, 20, 30]);
-        var modified = series.map(v => v * 2);
+        const series = new Series([10, 20, 30]);
+        const modified = series.map(v => v * 2);
         expect(modified.toArray()).to.eql([20, 40, 60]);
     });
 
     it('map ignores index', () => {
 
-        var series = new Series({
+        const series = new Series({
             values: [10, 20, 30],
             index: [100, 200, 300],
         });
-        var modified = series.map(v => v * 2);
+        const modified = series.map(v => v * 2);
         expect(modified.toPairs()).to.eql([[100, 20], [200, 40], [300, 60]]);
         expect(modified.getIndex().toArray()).to.eql([100, 200, 300]);
     });
     
 	it('can map', function () {
-		var series = new Series({ index: [0, 1, 2, 3], values: [100, 300, 200, 5] });
-		var modified = series.map((value, index) => value + 10);
+		const series = new Series({ index: [0, 1, 2, 3], values: [100, 300, 200, 5] });
+		const modified = series.map((value, index) => value + 10);
 		expect(modified.getIndex().toArray()).to.eql([0, 1, 2, 3]);
 		expect(modified.toArray()).to.eql([110, 310, 210, 15]);		
 	});
 
 	it('can can map - with array', function () {
-		var series = new Series({ index: [0, 1], values: [10, 20] });
-		var modified = series.flatMap(value => [100, 200]);
+		const series = new Series({ index: [0, 1], values: [10, 20] });
+		const modified = series.flatMap(value => [100, 200]);
 		expect(modified.getIndex().toArray()).to.eql([0, 0, 1, 1]);
 		expect(modified.toArray()).to.eql([100, 200, 100, 200]);
 	});
 
 	it('can flat map - with series', function () {
-		var series = new Series({ index: [0, 1], values: [100, 300] });
-		var modified = series.flatMap(value => new Series([0, 1]));
+		const series = new Series({ index: [0, 1], values: [100, 300] });
+		const modified = series.flatMap(value => new Series([0, 1]));
 		expect(modified.getIndex().toArray()).to.eql([0, 0, 1, 1]);
 		expect(modified.toArray()).to.eql([0, 1, 0, 1]);
 	});
 
 	it('can flat map - with data-frame', function () {
-		var series = new Series({ index: [0, 1], values: [100, 300] });
-		var modified = series.flatMap(value => new DataFrame([{ Value: 0 }, { Value: 1 }]));
+		const series = new Series({ index: [0, 1], values: [100, 300] });
+		const modified = series.flatMap(value => new DataFrame([{ Value: 0 }, { Value: 1 }]));
 		expect(modified.getIndex().toArray()).to.eql([0, 0, 1, 1]);
 		expect(modified.toArray()).to.eql([{ Value: 0 }, { Value: 1 }, { Value: 0 }, { Value: 1 }]);
 	});
